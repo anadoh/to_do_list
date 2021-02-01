379072326395f5412c5da660e8698c1f245f8470
anadoh <68846949+anadoh@users.noreply.github.com>
2021-02-01 14:31:49 +0100

    add key press, background

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -8,12 +8,12 @@ class Todo extends React.Component {
       { id: "222422", isCompleted: false, title: "water the plants" },
     ],
     inputValue: "",
-  }
+  };
 
   markCompleted(id) {
     const index = this.state.elements.findIndex((x) => x.id == id);
     const newElements = this.state.elements;
-    newElements[index].isCompleted = true;
+    newElements[index].isCompleted = !this.state.elements[index].isCompleted;
     this.setState({ elements: newElements });
   }
 
@@ -32,13 +32,20 @@ class Todo extends React.Component {
     this.setState({ inputValue: newValue });
   }
 
+  handleKeyPress(e) {
+    if (e.key === "Enter") {
+      console.log(this);
+      this.addItem();
+    }
+  }
+
   render() {
     const elements = this.state.elements.map((item) => {
       return (
         <TodoItem element={item} markClicked={this.markCompleted.bind(this)} />
       );
-    })
-    
+    });
+
     return (
       <div class="main">
         <h2>To do List</h2>
@@ -46,6 +53,7 @@ class Todo extends React.Component {
           type="text"
           value={this.state.inputValue}
           onChange={this.inputHandler.bind(this)}
+          onKeyUp={this.handleKeyPress.bind(this)}
         />
         <button onClick={this.addItem.bind(this)}>Add</button>
         {elements}
